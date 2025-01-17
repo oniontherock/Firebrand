b1d0adffad288dd08a652bbe5f58818c5bc1f109
oniontherock <orionmartin049@gmail.com>
2025-01-17 14:05:03 -0500

    got floor generation working well

--- Firebrand/Include/Game/World/Procedural Generation/Structure Generation/Floor Generation/FloorGenerator.cpp
+++ Firebrand/Include/Game/World/Procedural Generation/Structure Generation/Floor Generation/FloorGenerator.cpp
@@ -38,107 +38,82 @@ FloorGrid FloorGenerator::floorsGenerate(RoomTypeGrid& roomTypeGrid, const sf::V
 
 			if (roomType != Null) {
 				floorGrid.cellSet(x, y, roomTypeInstance.floorType);
+				continue;
 			}
-			else {
 
-				RoomType neighborType = RoomType::Null;
+			RoomType neighborType = RoomType::Null;
 
-				uint16_t i = 0;
-				for (int16_t offsetX = -1; offsetX <= 1; offsetX++) {
-					for (int16_t offsetY = -1; offsetY <= 1; offsetY++) {
+			for (int16_t offsetX = -1; offsetX <= 1; offsetX++) {
+				for (int16_t offsetY = -1; offsetY <= 1; offsetY++) {
 
-						// skip if offset is not even (I.E. diagonal or 0,0)
-						if (Mathi32::isEven(abs(offsetX + offsetY))) continue;
+					// skip if offset is even (I.E. diagonal or 0,0)
+					if (Mathi32::isEven(abs(offsetX + offsetY))) continue;
 					
-						i++;
 
-						int16_t offsettedX = int16_t(x) + offsetX;
-						int16_t offsettedY = int16_t(y) + offsetY;
+					int16_t offsettedX = int16_t(x) + offsetX;
+					int16_t offsettedY = int16_t(y) + offsetY;
 
-						// skip if cell not in floor grid
-						if (!floorGrid.cellPosIsInGrid(offsettedX, offsettedY)) continue;
+					// skip if cell not in floor grid
+					if (!floorGrid.cellPosIsInGrid(offsettedX, offsettedY)) continue;
 
-						// get neighbor's position as a structureCell
-						sf::Vector2u neighborStructureCellPos = sf::Vector2u(trunc(offsettedX / 2u), trunc(offsettedY / 2u));
-						// if the neighbor's structure cell pos is the same as ours, skip
-						if (structureCellPos == neighborStructureCellPos) continue;
+					// get neighbor's position as a structureCell
+					sf::Vector2u neighborStructureCellPos = sf::Vector2u(trunc(offsettedX / 2u), trunc(offsettedY / 2u));
+					// if the neighbor's structure cell pos is the same as ours, skip
+					if (structureCellPos == neighborStructureCellPos) continue;
 
-						RoomType neighborTypeCur = roomTypeGrid.cellGet(neighborStructureCellPos).type;
+					RoomType neighborTypeCur = roomTypeGrid.cellGet(neighborStructureCellPos).type;
 
-						// skip if neighbor type is null
-						if (neighborTypeCur == Null) continue;
+					// skip if neighbor type is null
+					if (neighborTypeCur == Null) continue;
 
-						neighborType = neighborTypeCur;
-						break;
-					}
-				}
-
-				if (neighborType != Null) {
-					RoomTypeInstance neighborRoomTypeInstance = RoomTypeRegistry::roomTypeInstanceGet(neighborType);
-
-					floorGrid.cellSet(x, y, neighborRoomTypeInstance.floorType);
+					neighborType = neighborTypeCur;
+					break;
 				}
 			}
 
+			if (neighborType != Null) {
+				RoomTypeInstance neighborRoomTypeInstance = RoomTypeRegistry::roomTypeInstanceGet(neighborType);
+
+				floorGrid.cellSet(x, y, neighborRoomTypeInstance.floorType);
+				continue;
+			}
 
 
-			//// get validity of all sides of the cell
-			//bool rightIsValid = roomTypeGrid.cellPosIsInGrid(x + 1, y);
-			//bool upIsValid = roomTypeGrid.cellPosIsInGrid(x, y - 1);
-			//bool leftIsValid = roomTypeGrid.cellPosIsInGrid(x - 1, y);
-			//bool downIsValid = roomTypeGrid.cellPosIsInGrid(x, y + 1);
+			RoomType neighborDiagonalType = RoomType::Null;
 
-			//// initialize types for all sides of the cell
-			//RoomType rightType = RoomType::Null;
-			//RoomType upType = RoomType::Null;
-			//RoomType leftType = RoomType::Null;
-			//RoomType downType = RoomType::Null;
+			for (int16_t offsetX = -1; offsetX <= 1; offsetX++) {
+				for (int16_t offsetY = -1; offsetY <= 1; offsetY++) {
 
-			//// get the type at a given side of the cell if it is valid
-			//if (rightIsValid) {
-			//	rightType = roomTypeGrid.cellGet(x + 1, y).type;
-			//}
-			//if (upIsValid) {
-			//	upType = roomTypeGrid.cellGet(x, y - 1).type;
-			//}
-			//if (leftIsValid) {
-			//	leftType = roomTypeGrid.cellGet(x - 1, y).type;
-			//}
-			//if (downIsValid) {
-			//	downType = roomTypeGrid.cellGet(x, y + 1).type;
-			//}
+					// skip if offset is odd (I.E. not diagonal), or skip  if offset is 0,0
+					if (Mathi32::isOdd(abs(offsetX + offsetY)) || (offsetX == 0 && offsetY == 0)) continue;
 
-			//// get floor types of every side
-			//std::string rightFloorType = RoomTypeRegistry::roomTypeInstanceGet(rightType).floorType;
-			//std::string upFloorType = RoomTypeRegistry::roomTypeInstanceGet(rightType).floorType;
-			//std::string leftFloorType = RoomTypeRegistry::roomTypeInstanceGet(rightType).floorType;
-			//std::string downFloorType = RoomTypeRegistry::roomTypeInstanceGet(rightType).floorType;
+					int16_t offsettedX = int16_t(x) + offsetX;
+					int16_t offsettedY = int16_t(y) + offsetY;
 
+					// skip if cell not in floor grid
+					if (!floorGrid.cellPosIsInGrid(offsettedX, offsettedY)) continue;
 
-			//// if there are no types up and down, mark floors horizontally
-			//if (upType == Null && downType == Null) {
-			//	floorMarkHorizontal(roomTypeGrid, floorGrid, structureSize, sf::Vector2u(x * 2, y * 2), leftFloorType, rightFloorType);
-			//}
-			//// if there are no types left and right, mark floors vertically
-			//else if (leftType == Null && rightType == Null) {
-			//	floorMarkVertical(roomTypeGrid, floorGrid, structureSize, sf::Vector2u(x * 2, y * 2), upFloorType, downFloorType);
-			//}
-			//// if one (or more) of the the up or down types are null, and neither left/right types are null, mark floor horizontally
-			//else if ((upType == Null || downType == Null) && (leftType != Null && rightType != Null)) {
-			//	floorMarkHorizontal(roomTypeGrid, floorGrid, structureSize, sf::Vector2u(x * 2, y * 2), leftFloorType, rightFloorType);
-			//}
-			//// if one (or more) of the the left or right types are null, and neither up/down types are null, mark floor vertically
-			//else if ((leftType == Null || rightType == Null) && (upType != Null && downType != Null)) {
-			//	floorMarkVertical(roomTypeGrid, floorGrid, structureSize, sf::Vector2u(x * 2, y * 2), leftFloorType, rightFloorType);
-			//}
-			//// if the up type isn't the same as the down type, mark floors horizontally
-			//else if (upType != downType && leftType == rightType) {
-			//	floorMarkHorizontal(roomTypeGrid, floorGrid, structureSize, sf::Vector2u(x * 2, y * 2), leftFloorType, rightFloorType);
-			//}
-			//// if the left type isn't the same as the right type, mark floors vertically
-			//else if (leftType != rightType && upType == downType) {
-			//	floorMarkVertical(roomTypeGrid, floorGrid, structureSize, sf::Vector2u(x * 2, y * 2), leftFloorType, rightFloorType);
-			//}
+					// get neighbor's position as a structureCell
+					sf::Vector2u neighborStructureCellPos = sf::Vector2u(trunc(offsettedX / 2u), trunc(offsettedY / 2u));
+					// if the neighbor's structure cell pos is the same as ours, skip
+					if (structureCellPos == neighborStructureCellPos) continue;
+
+					RoomType neighborTypeCur = roomTypeGrid.cellGet(neighborStructureCellPos).type;
+
+					// skip if neighbor type is null
+					if (neighborTypeCur == Null) continue;
+
+					neighborDiagonalType = neighborTypeCur;
+					break;
+				}
+			}
+
+			if (neighborDiagonalType != Null) {
+				RoomTypeInstance neighborRoomTypeInstance = RoomTypeRegistry::roomTypeInstanceGet(neighborDiagonalType);
+
+				floorGrid.cellSet(x, y, neighborRoomTypeInstance.floorType);
+				continue;
+			}
 		}
 	}
 
